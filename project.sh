a6e25c3c886b966115c9943c11375b576d916576
Anson Inomwan <inomwanosaruese@gmail.com>
2025-11-05 18:19:54 -0500

    added project.sh

---
+++ project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Welcome to the Anson's Bash Toolkit!"
+echo "To Mr Prof"
+
+# Ask for the user's name
+echo "What is your name?"
+read name
+
+# Greet the user
+echo "Hi $name! Let's do a few quick tasks together."
+
+# Ask for the user's age
+echo "How old are you?"
+read age
