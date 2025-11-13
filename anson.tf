ee6e79e0f13ac89014f1994283b84d22a318c5c0
Anson Inomwan <inomwanosaruese@gmail.com>
2025-11-12 21:39:23 -0500

    added anson.tf to repo

--- anson.tf
+++ anson.tf
@@ -1,4 +1,30 @@
-code
-ls list
-cd create dir
-rm remove dir 
+anson@SnifflyAnson:~/ghost$ ./power.sh
+hello what is your name
+anson
+hello anson
+
+what are you doing
+football
+you are playing football?
+anson@SnifflyAnson:~/ghost$ chomd u+x power.sh
+Command 'chomd' not found, did you mean:
+  command 'chmod' from deb coreutils (9.4-2ubuntu2)
+Try: sudo apt install <deb name>
+anson@SnifflyAnson:~/ghost$ chmod u+x power.sh
+anson@SnifflyAnson:~/ghost$ chmod +x ghost35.txt
+chmod: cannot access 'ghost35.txt': No such file or directory
+anson@SnifflyAnson:~/ghost$ chmod +x power.sh
+anson@SnifflyAnson:~/ghost$ ls -l pwoer.sh
+ls: cannot access 'pwoer.sh': No such file or directory
+anson@SnifflyAnson:~/ghost$ ls -l power.sh
+-rwxr-xr-x 1 anson anson 149 Nov 12 21:21 power.sh
+anson@SnifflyAnson:~/ghost$ chmod u+x power.sh
+anson@SnifflyAnson:~/ghost$ ls -l power.sh
+-rwxr-xr-x 1 anson anson 149 Nov 12 21:21 power.sh
+anson@SnifflyAnson:~/ghost$ chmod 644 power.sh
+anson@SnifflyAnson:~/ghost$ ls -l power.sh
+-rw-r--r-- 1 anson anson 149 Nov 12 21:21 power.sh
+anson@SnifflyAnson:~/ghost$ chmod 744 power.sh
+anson@SnifflyAnson:~/ghost$ ls -l power.sh
+-rwxr--r-- 1 anson anson 149 Nov 12 21:21 power.sh
+anson@SnifflyAnson:~/ghost$
