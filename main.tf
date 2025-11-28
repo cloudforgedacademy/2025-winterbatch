de57092a781eaf585af25c5019993b0b13eebbbf
Anson Inomwan <inomwanosaruese@gmail.com>
2025-11-27 20:21:34 -0500

    fixed main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,17 @@ resource "azurerm_resource_group" "anson3511" {
   name     = "anson3511"
   location = "canadacentral"
 }
+
 resource "azurerm_data_factory" "ebun2511" {
   name                = "ebun2511"
- location            = azurerm_resource_group.anson3511.location
+  location            = azurerm_resource_group.anson3511.location
   resource_group_name = azurerm_resource_group.anson3511.name
+
   tags = {
     environment = "dev"
   }
 }
+
 terraform {
   required_providers {
     azurerm = {
@@ -21,5 +24,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "3bbbef12-5553-4245-b63a-765d4c79bd1d"
+  subscription_id = "3bbbfe12-5553-4245-b63a-765d4c79bd1d"
 }
