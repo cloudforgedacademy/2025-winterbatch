e2de4edc483a6e9dd73cb571ccc3a7d4df64ce8c
anson inomwan <inomwanosaruese@gmail.com>
2026-01-11 20:45:59 -0500

    webserver to repo

--- main.tf
+++ main.tf
@@ -109,3 +109,18 @@ resource "azurerm_windows_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_virtual_machine_extension" "web_server_install" {
+  name                       = "web-server-install"
+  virtual_machine_id         = azurerm_windows_virtual_machine.vm.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell -Command \\"Install-WindowsFeature -name Web-Server -IncludeManagementTools\\""
+    }
+  SETTINGS
+}
